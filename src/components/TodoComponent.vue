d7b6ec67561e44b718e11f37efdedc51b53f8a9b
Prashant Bhate <prashant.bhate@gmail.com>
2024-12-06 11:45:58 +0530

    Refactored TodoTable

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -1,13 +1,16 @@
 <script setup>
-import InputWithError from './InputWithError.vue';
+import TodoForm from './TodoForm.vue';
 import { ref, computed } from 'vue'
+import TodoTable from './TodoTable.vue';
 
 const todos = ref([{
+    id: '1',
     user: 'abcd',
     task: 'task123',
     done: false,
     targetDate: '12/12/2024'
 }, {
+    id: '2',
     user: 'abcd',
     task: 'task123',
     done: false,
@@ -24,7 +27,42 @@ const loading = ref(false)
 const error = ref(null)
 const validationErrors = ref({})
 
-const today = computed(() => new Date().toISOString().split('T')[0])
+const addTodo = async () => {
+    console.log("Adding newTodo", newTodo.value)
+    resetForm()
+}
+const updateTodo = async () => {
+    console.log("updating editingTodo", editingTodo.value)
+    cancelEdit()
+}
+const deleteTodo = async (id) => {
+    console.log("deleting id", id)
+    cancelEdit()
+}
+const resetForm = () => {
+    newTodo.value = {
+        user: '',
+        task: '',
+        done: false,
+        targetDate: ''
+    }
+    validationErrors.value = {}
+}
+const editTodo = (id) => {
+    console.log("edit id", id)
+    const index = todos.value.findIndex(todo => todo.id === id)
+    if (index !== -1) {
+        const todo = todos.value[index]
+        const todoCopy = { ...todo }
+        console.log(todoCopy)
+        editingTodo.value = todoCopy // Create a copy to avoid direct mutations
+    }
+}
+
+const cancelEdit = () => {
+    editingTodo.value = null
+    validationErrors.value = {}
+}
 
 
 </script>
@@ -32,56 +70,13 @@ const today = computed(() => new Date().toISOString().split('T')[0])
     <section id="todoform">
         <h2>Todo View</h2>
         <div v-if="error" class="error">{{ error }}</div>
-        <form v-if="!editingTodo" @submit.prevent="addTodo">
-            <fieldset>
-                <legend>Add: Todo</legend>
-                <InputWithError inputId="Username" label="Username" v-model:task="newTodo.user" v-model:error="validationErrors.user" placeholder="Username" />
-                <InputWithError inputId="Task" label="Task" v-model:task="newTodo.task" v-model:error="validationErrors.task" placeholder="Task (10-200 chars)" type="textarea" />
-                <InputWithError inputId="Date" label="Date" v-model:task="newTodo.targetDate" v-model:error="validationErrors.targetDate" type="date" :min="today" />
-            </fieldset>
-            <fieldset class=".grid">
-                <button type="submit" class="outline" :aria-busy="loading" :disabled="loading">Add Todo</button>
-                <button type="button" @click="resetForm">Reset</button>
-            </fieldset>
-        </form>
-        <form v-else @submit.prevent="updateTodo">
-            <fieldset>
-                <legend>Edit: Todo</legend>
-                <InputWithError inputId="Username" label="Username" v-model:task="editingTodo.user" v-model:error="validationErrors.user" placeholder="Username" />
-                <InputWithError inputId="Task" label="Task" v-model:task="editingTodo.task" v-model:error="validationErrors.task" placeholder="Task (10-200 chars)" type="textarea" />
-                <InputWithError inputId="Date" label="Date" v-model:task="editingTodo.targetDate" v-model:error="validationErrors.targetDate" type="date" :min="today" />
-            </fieldset>
-            <fieldset class=".grid">
-                <button type="submit" :aria-busy="loading" :disabled="loading">Update Todo</button>
-                <button type="button" @click="cancelEdit">Cancel</button>
-            </fieldset>
-        </form>
+        <TodoForm v-if="!editingTodo" :loading="loading" v-model:todo="newTodo" v-model:validationErrors="validationErrors" form-label="Add: Todo" submit-label="Add Todo" reset-label="Reset" @submit="addTodo" @reset="resetForm" />
+        <TodoForm v-else :loading="loading" v-model:todo="editingTodo" v-model:validationErrors="validationErrors" form-label="Edit: Todo" submit-label="Update Todo" reset-label="Cancel" @submit="updateTodo" @reset="cancelEdit" />
     </section>
     <section id="todotable">
         <h2>Todo List</h2>
         <div>
-            <table v-if="todos.length">
-                <thead>
-                    <tr>
-                        <th>User</th>
-                        <th>Task</th>
-                        <th>Date</th>
-                        <th>Done?</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="todo in todos" :key="todo.id">
-                        <td>{{ todo.user }} </td>
-                        <td>{{ todo.task }}</td>
-                        <td>{{ todo.targetDate }}</td>
-                        <td><input type="checkbox" v-model="todo.done" /></td>
-                        <td><button @click="editTodo(todo)">Edit</button></td>
-                        <td><button @click="deleteTodo(todo.id)">Delete</button></td>
-                    </tr>
-                </tbody>
-            </table>
+            <TodoTable :loading="loading" :todos="todos" @edit-click="editTodo" @remove-click="deleteTodo" />
         </div>
     </section>
 </template>
